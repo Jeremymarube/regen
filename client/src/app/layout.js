060bca6ca589c017210be3360919edb741c09c41
laurenannmurugi <laurenann1871@gmail.com>
2025-10-22 22:45:40 +0300

    Update the dashboard

--- client/src/app/layout.js
+++ client/src/app/layout.js
@@ -17,7 +17,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${rasa.variable} font-sans antialiased bg-gray-50`}
+        className={`${rasa.variable} font-rasa antialiased bg-gray-50`}
       >
         <AuthProvider>
           {children}
