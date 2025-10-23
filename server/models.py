b3201337ecaa14c28940442b3d05c526949e07a7
Jeremy Marube <jeremymarube1@gmail.com>
2025-10-23 15:50:17 +0300

    update-client/src/app/dashboard/manage-centers/page.jsx

---
+++ server/models.py
@@ -0,0 +1,14 @@
+from database import db
+
+class RecyclingCenter(db.Model):
+    __tablename__ = 'recycling_centers'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), nullable=False)
+    location = db.Column(db.String(255), nullable=False)
+    latitude = db.Column(db.Float, nullable=True)
+
+
+
+    def __repr__(self):
+        return f"<RecyclingCenter {self.name}>"
