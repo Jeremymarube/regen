9392b0c7148130b02d9e63739fa567ee0188c97c
Jeremy Marube <jeremymarube1@gmail.com>
2025-10-23 15:14:47 +0300

    Refactor database.py to remove unused SQLAlchemy initialization and streamline database setup

--- server/database.py
+++ server/database.py
@@ -1,11 +1,17 @@
-from flask_sqlalchemy import SQLAlchemy
+#from flask_sqlalchemy import SQLAlchemy
 
-db = SQLAlchemy()
+# db = SQLAlchemy()
+
+# def init_db(app):
+#     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///regen.db'
+#     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+#     db.init_app(app)
+    
+#     with app.app_context():
+#         db.create_all()
+from extensions import db  # Import from extensions
 
 def init_db(app):
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///regen.db'
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    db.init_app(app)
-    
+    # No need for db.init_app(app) here since it's done in app.py
     with app.app_context():
         db.create_all()
