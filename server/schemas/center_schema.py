ea1dc678022257150c20f2e74049391499b903ea
Jeremy Marube <jeremymarube1@gmail.com>
2025-10-23 16:34:10 +0300

    updating file content

--- server/schemas/center_schema.py
+++ server/schemas/center_schema.py
@@ -4,3 +4,6 @@ class RecyclingCenterSchema(Schema):
     id = fields.Int(dump_only=True)
     name = fields.Str(required=True)
     location = fields.Str(required=True)
+    latitude = fields.Float(allow_none=True)
+    longitude = fields.Float(allow_none=True)
+    facility_type = fields.Str(required=True)
