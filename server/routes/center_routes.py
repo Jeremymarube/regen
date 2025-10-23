bd0d835fe79791241dc4802eaa92cf2c3a756fb0
Jeremy Marube <jeremymarube1@gmail.com>
2025-10-23 16:50:40 +0300

    implemeting schema in routes

--- server/routes/center_routes.py
+++ server/routes/center_routes.py
@@ -2,3 +2,8 @@ from flask import Blueprint, request, jsonify
 from models import RecyclingCenter
 from database import db
 from schemas.center_schema import RecyclingCenterSchema
+
+center_bp = Blueprint('centers', __name__, url_prefix='/api/recycling-centers')
+
+center_schema = RecyclingCenterSchema()
+centers_schema = RecyclingCenterSchema(many=True)
