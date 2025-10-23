1d768080f8be2a3a0b59e98d6ca64aaecd1a6fe8
Jeremy Marube <jeremymarube1@gmail.com>
2025-10-23 16:36:25 +0300

    complete centers_schema.py

--- server/schemas/center_schema.py
+++ server/schemas/center_schema.py
@@ -7,3 +7,7 @@ class RecyclingCenterSchema(Schema):
     latitude = fields.Float(allow_none=True)
     longitude = fields.Float(allow_none=True)
     facility_type = fields.Str(required=True)
+    contact = fields.Str(allow_none=True)
+    operating_hours = fields.Str(allow_none=True)
+    accepted_types = fields.List(fields.Str(), required=False)
+    is_active = fields.Bool()
