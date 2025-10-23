a88409ef7df5f8f189daa7cfded281d5006ccc92
Jeremy Marube <jeremymarube1@gmail.com>
2025-10-23 18:08:34 +0300

    Initialize Flask-Migrate and import RecyclingCenter model

--- server/app.py
+++ server/app.py
@@ -1,11 +1,23 @@
 from flask import Flask
-from database import init_db, db
-from routes import center_bp
+from flask_migrate import Migrate
+from database import db
+from routes.center_routes import center_bp
+from models import RecyclingCenter
 
 app = Flask(__name__)
 app.config.from_object('config.Config')
 
 db.init_app(app)
 
-# register blueprints
+# Initialize Flask-Migrate
+migrate = Migrate(app, db)
+
+# Register blueprints
 app.register_blueprint(center_bp)
+
+@app.route('/')
+def home():
+    return {'message': 'ReGen Backend is running'}
+
+if __name__ == '__main__':
+    app.run(debug=True)
