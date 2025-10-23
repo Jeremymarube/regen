9d8bdb5c89115a16e2638b3c0d9ce44e0a76e95b
Jeremy Marube <jeremymarube1@gmail.com>
2025-10-23 17:02:17 +0300

    registering the blueprint with routes for centers

---
+++ server/app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+from database import db
+from routes import center_bp
+
+app = Flask(__name__)
+app.config.from_object('config.Config')
+
+db.init_app(app)
+
+# register blueprints
+app.register_blueprint(center_bp)
