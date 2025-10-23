e07b6a1578da89249485130541046632689aa0e7
James-Patrick416 <jp1252211@gmail.com>
2025-10-23 01:23:15 -0700

    modified the create a new user object with a unique UUID,setting wmail and optional fields

--- server/routes/auth_routes.py
+++ server/routes/auth_routes.py
@@ -16,7 +16,7 @@ def register():
         if User.query.filter_by(email=data['email']).first():
             return jsonify({'message': 'User already exists'}), 400
         
-        user = User(
+        user = User(     # created a new user object with a unique UUID, setting email and optional fields
             id=str(uuid.uuid4()),
             name=data.get('name', ''),
             email=data['email'],
