4e26dabd844a239e016ba45150d3612fb0b1ea1c
Jeremy Marube <jeremymarube1@gmail.com>
2025-10-27 13:28:23 +0300

    fix: Update layout structure and styling in DashboardLayout component

--- client/src/app/dashboard/layout.jsx
+++ client/src/app/dashboard/layout.jsx
@@ -5,14 +5,16 @@ export default function DashboardLayout({ children }) {
     <div className="flex min-h-screen bg-[#F9FAFB] ">
       {/* Sidebar */}
       <Sidebar />
-      {/* <main className="flex-1 ml-[180px] pl-3 pr-8 py-10">
-  <div className="max-w-7xl mx-auto">
-    {children}
-  </div>
-</main> */}
-     <main className="flex-1 ml-[180px] pl-4 pr-12 py-10">
+     {/* <main className="flex-1 ml-[180px] pl-4 pr-12 py-10">
   <div className="max-w-7xl mx-auto">{children}</div>
 </main>
-    </div> 
+    </div>  */}
+    <main className="flex-1 ml-[180px] px-8 py-8" style={{ zoom: '0.9' }}>
+  <div className="max-w-7xl mx-auto w-full">{children}</div>
+</main>
+    </div>
+
   );
 }
+
+
