fa7203fd7fafc15118f8b5fafce11acf91506170
Jeremy Marube <jeremymarube1@gmail.com>
2025-10-28 11:15:27 +0300

    fix: Refactor Sidebar rendering and adjust main layout for consistency

--- client/src/app/dashboard/layout.jsx
+++ client/src/app/dashboard/layout.jsx
@@ -4,12 +4,11 @@ export default function DashboardLayout({ children }) {
   return (
     <div className="flex min-h-screen bg-[#F9FAFB] ">
       {/* Sidebar */}
+      <div className="hidden lg:block">
       <Sidebar />
-     {/* <main className="flex-1 ml-[180px] pl-4 pr-12 py-10">
-  <div className="max-w-7xl mx-auto">{children}</div>
-</main>
-    </div>  */}
-    <main className="flex-1 ml-[180px] px-8 py-8" style={{ zoom: '0.9' }}>
+      </div>
+     
+    <main className="flex-1 lg:ml-[180px] px-4 lg:px-8 py-8" style={{ zoom: '0.9' }}>
   <div className="max-w-7xl mx-auto w-full">{children}</div>
 </main>
     </div>
